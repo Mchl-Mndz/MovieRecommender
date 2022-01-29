f859ef824af0c36da22244060539ece63e5894c7
mchl-mndz <michael.mendez@ufl.edu>
2022-01-29 15:29:02 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Swamphacks2022.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Swamphacks2022.dir/main.cpp.o"
+  "Swamphacks2022"
+  "Swamphacks2022.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Swamphacks2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
