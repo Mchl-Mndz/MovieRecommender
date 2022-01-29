94f3c7fc856267113f09b55e1c54de7fe35b4ba9
mchl-mndz <michael.mendez@ufl.edu>
2022-01-29 15:54:46 -0500

    Recommendhandler added

---
+++ RecommendHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael Mendez on 1/29/22.
+//
+
+#include "RecommendHandler.h"
---
+++ RecommendHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by Michael Mendez on 1/29/22.
+//
+
+#ifndef SWAMPHACKS2022_RECOMMENDHANDLER_H
+#define SWAMPHACKS2022_RECOMMENDHANDLER_H
+
+
+class RecommendHandler {
+
+};
+
+
+#endif //SWAMPHACKS2022_RECOMMENDHANDLER_H
